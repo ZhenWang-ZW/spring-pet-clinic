6856858392150b2fc55dbb6de3cded8c01d1e4b5
Zhen Wang <zhen446@hotmail.com>
2020-06-04 15:04:12 +0100

    project init

---
+++ src/main/java/guru/springframework/zwpetclinic/ZwPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.zwpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZwPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/zwpetclinic/ZwPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.zwpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
