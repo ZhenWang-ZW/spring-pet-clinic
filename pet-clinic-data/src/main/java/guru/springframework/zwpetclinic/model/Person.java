63b8890310e835755ccc16c4701b29fe7ccdeb8b
Zhen Wang <zhen446@hotmail.com>
2020-06-04 22:16:51 +0100

    Add id for classes. Closes #10.

--- pet-clinic-data/src/main/java/guru/springframework/zwpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/zwpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package guru.springframework.zwpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/guru/springframework/zwpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/zwpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package guru.springframework.zwpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
