6243a7fba9ccceaf115f56f0ee65c80e40870bcf
Zhen Wang <zhen446@hotmail.com>
2020-06-05 13:59:10 +0100

    impl map services. Closes #6.

---
+++ pet-clinic-data/src/main/java/guru/springframework/zwpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package guru.springframework.zwpetclinic.services.map;
+
+import guru.springframework.zwpetclinic.model.Pet;
+import guru.springframework.zwpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+}
