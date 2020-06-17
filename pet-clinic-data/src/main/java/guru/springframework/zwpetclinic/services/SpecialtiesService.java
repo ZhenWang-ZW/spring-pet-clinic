63bfb21faf640fbae352d52c61b0d5b666d9cfaf
Zhen Wang <zhen446@hotmail.com>
2020-06-18 00:10:05 +0100

    Create specialty map service impl. Closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/zwpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.zwpetclinic.services;
+
+import guru.springframework.zwpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
