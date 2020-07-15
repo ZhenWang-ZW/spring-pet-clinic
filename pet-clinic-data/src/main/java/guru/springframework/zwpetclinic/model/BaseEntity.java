c001570ba81652bdead8665ab3b555cd1fd1eaf1
Zhen Wang <zhen446@hotmail.com>
2020-07-15 23:12:15 +0100

    refactor models to lombok

--- pet-clinic-data/src/main/java/guru/springframework/zwpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/zwpetclinic/model/BaseEntity.java
@@ -1,22 +1,25 @@
 package guru.springframework.zwpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class BaseEntity implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+    public boolean isNew() {
+        return this.id == null;
     }
 }
--- pet-clinic-data/src/main/java/guru/springframework/zwpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/zwpetclinic/model/Pet.java
@@ -1,55 +1,50 @@
 package guru.springframework.zwpetclinic.model;
 
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity{
-    @Column(name="name")
+
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+
+        if (visits == null || visits.size() > 0 ) {
+            this.visits = visits;
+        }
+    }
+
+    @Column(name = "name")
     private String name;
+
     @ManyToOne
-    @JoinColumn(name="type_id")
-    private PetType type;
+    @JoinColumn(name = "type_id")
+    private PetType petType;
+
     @ManyToOne
-    @JoinColumn(name="owner_id")
+    @JoinColumn(name = "owner_id")
     private Owner owner;
-    @Column(name="dob")
-    private LocalDate dob;
+
+    @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthDate;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public PetType getType() {
-        return type;
-    }
-
-    public void setType(PetType type) {
-        this.type = type;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
 }
